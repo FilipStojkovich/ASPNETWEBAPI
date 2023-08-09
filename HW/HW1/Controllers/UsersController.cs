2d71ebca39f0975550c28d4b93a25dd4d2afd242
FilipStojkovich <119892777+FilipStojkovich@users.noreply.github.com>
2023-08-09 15:05:08 +0200

    Add files via upload

---
+++ HW/HW1/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW1.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<List<string>> GetAll()
+		{
+			return Ok(StaticDb.Users);
+		}
+
+		[HttpGet("{index}")]
+		public ActionResult<string> GetUserByIndex(int index)
+		{
+			try
+			{
+				if(index < 0)
+				{
+					return StatusCode(StatusCodes.Status400BadRequest, "The index is a negative number");
+				}
+
+				if(index >= StaticDb.Users.Count)
+				{
+					return StatusCode(StatusCodes.Status404NotFound, $"The user with index {index} was not found.");
+				}
+
+				return Ok(StaticDb.Users[index]);
+			}
+			catch(Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "An error occured. Contact the support team.");
+			}
+		}
+	}
+}
---
+++ HW/HW1/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace HW1
+{
+	public static class StaticDb
+	{
+		public static List<string> Users = new List<string>
+		{
+			"Filip",
+			"Bojan",
+			"Tosho",
+			"Kostadin",
+			"Damjan"
+		};
+	}
+}
